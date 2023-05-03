2a3ed181df57f4584fd19b85bfd270349d5b87a7
Robin Moretti <robinhenrimoretti@gmail.com>
2023-05-03 21:48:07 +0200

    make the trail rendering

--- Game-unity/Assets/Project/Scripts/Controls/SwipeDetector.cs
+++ Game-unity/Assets/Project/Scripts/Controls/SwipeDetector.cs
@@ -8,10 +8,14 @@ public class SwipeDetector : MonoBehaviour
     private float startTime, endTime;
     private InputManager inputManager;
 
+
+
     
     [SerializeField]private float minimumDistance = 0.2f;
     [SerializeField]private float maximumTime = 0.8f;
 
+    [SerializeField] private GameObject trail;
+    [SerializeField] private GameObject ball;
 
     private void Awake() {
         inputManager = InputManager.Instance;
@@ -29,18 +33,36 @@ public class SwipeDetector : MonoBehaviour
     private void SwipeStart(Vector3 position, float time){
         startPosition = position;
         startTime = time;
+        trail.transform.position = inputManager.PrimaryPosition();
+        trail.GetComponent<TrailRenderer>().Clear();
+        StartCoroutine("trailUpdate");
+
     }
     private void SwipeEnd(Vector3 position, float time){
         endPosition = position;
         endTime = time;
         DetectSwipe();
+        StopCoroutine("trailUpdate");
+    }
+
+    IEnumerator trailUpdate(){
+        while(true){
+            yield return new WaitForFixedUpdate();
+            trail.transform.position = inputManager.PrimaryPosition();
+        }
     }
 
     private void DetectSwipe(){
         if(Vector3.Distance(startPosition, endPosition) >= minimumDistance &&
          (endTime-startTime) <= maximumTime){
             Debug.Log("swiped = " );
+            Vector3 Direction = endPosition - startPosition;
             Debug.DrawLine(startPosition, endPosition, Color.blue, 5f);
+
+            ball.GetComponent<Rigidbody>().AddExplosionForce(
+                Vector2.Distance(startPosition, endPosition), 
+                ball.transform.position, 1);
+
         }
     }
 }
--- Game-unity/Assets/Project/Scripts/Utils.cs
+++ Game-unity/Assets/Project/Scripts/Utils.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Utils : MonoBehaviour
 {
     public static Vector3 ScreenToWorld(Camera camera, Vector3 position){
-        position.z = camera.nearClipPlane;
+        position.z = camera.nearClipPlane + 1;
         position = camera.ScreenToWorldPoint(position);
         return position;
     }
