d5d7f1400d94a934bc6f143cf27b701fed5e5e2d
Robin Moretti <robinhenrimoretti@gmail.com>
2023-05-17 18:13:36 +0200

    Stop the ball when its too small

--- Game-unity/Assets/Project/Scripts/BallController.cs
+++ Game-unity/Assets/Project/Scripts/BallController.cs
@@ -8,14 +8,40 @@ public class BallController : MonoBehaviour
     [SerializeField] private ForceMode forceMode;
 
     private new Rigidbody rigidbody;
+    private bool wasShoot = false;
 
     private void Start() {
         rigidbody = GetComponent<Rigidbody>();
     }
 
     public void shot(Vector3 direction, float strenght){
+        if(wasShoot == false){
+            direction.y = 0;
+            rigidbody.AddForce(direction * strenght * shotStrenght, forceMode);
+        }
 
-        direction.y = 0;
-        rigidbody.AddForce(direction * strenght * shotStrenght, forceMode);
+        StartCoroutine(surveilBallSpeed());
+        wasShoot = true;
     }
+
+    IEnumerator surveilBallSpeed(){
+        yield return new WaitForSeconds(0.2f);
+        
+        while(rigidbody.velocity.magnitude > 0.10f){
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        // stop the ball
+        while(rigidbody.velocity.magnitude > 0.001f){
+            rigidbody.velocity *= 0.7f;
+            rigidbody.angularVelocity *= 0.7f;
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        wasShoot = false;
+
+        yield return null;
+    }
+
+
 }
